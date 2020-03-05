2da81478c13a1a6feb24b3ece0b31a7758d8481b
martins0n <martinmail@mail.ru>
2020-03-05 20:24:45 +0300

    NEW: tests

---
+++ tests/test_methods.py
@@ -0,0 +1,15 @@
+import pytest
+import pandas as pd
+import numpy as np
+from pym4metalearning import m4meta_train, make_prediction
+
+
+def test_m4meta_train():
+    m4meta_train("/dev/null")
+
+def test_make_prediction():
+    N = 100
+    horizon = 10
+    ts_to_test = pd.Series(np.arange(N), index=pd.date_range(start="2001-01-01", freq="D", periods=N))
+    prediction = make_prediction(ts_to_test, "./tests/data/meta_model.Rdata", h=horizon)
+    assert (len(prediction) == horizon)
